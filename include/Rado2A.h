d86a137ae13b264f2f8415fa301b6cea2e192c11
Emil Sharipov <emil.sharipov@radiogigabit.com>
2024-03-29 14:34:48 +0300

    add Rado nu calculate

--- include/Rado2A.h
+++ include/Rado2A.h
@@ -13,47 +13,51 @@ class Redo2A {
 
     std::vector<std::vector<double>> kVec;
 
-    std::vector<double> calc_next_k_vec(double h, double t_current, const std::vector<double>& x_current_vec, 
+    std::vector<double> calculate_next_nu_vec(double h, double t_current, const std::vector<double>& x_current_vec, 
                         const std::vector<std::function<double(double, const std::vector<double>&)>>& F,
-                        const std::vector<std::vector<std::function<double(double, const std::vector<double>&)>>>& J)
+                        const std::vector<std::vector<std::function<double(double, const std::vector<double>&)>>>& J, double eps)
     {
         std::vector<double> cCoefs{1/3, 1};
         std::vector<std::vector<double>> aCoefs{{5/12, -1/12}, {3/4, 1/4}};
 
-        const std::vector<std::function<double(double, const std::vector<double>&)>> F1;
-        const std::vector<std::vector<std::function<double(double, const std::vector<double>&)>>> J1;
+        std::vector<std::function<double(const std::vector<double>&)>> F1;
+        std::vector<std::vector<std::function<double(const std::vector<double>&)>>> J1;
 
         for(std::size_t i = 0; i < F.size(); ++i) {
             for(std::size_t j = 0; j < cCoefs.size(); ++j) {
-                F1.push_back([i, j, &F, &cCoefs, h, &x_current_vec, &aCoefs](const std::vector<double>& nu){
+                F1.push_back([i, j, &F, &cCoefs, h, &x_current_vec, &aCoefs, &t_current](const std::vector<double>& nu){
                     std::vector<double> x_new;
                     for(std::size_t i1 = 0; i1 < F.size(); ++i1) {
                         double sum = 0;
                         for(std::size_t j1 = 0; j1 < cCoefs.size(); ++j1) {
-                            sum += aCoefs[j][j1]*nu[]
+                            sum += aCoefs[j][j1]*nu[i1 * cCoefs.size() + j1];
                         }
+                        x_new.push_back(x_current_vec[i1] + h * sum);
                     }
+                    return F[i](t_current + cCoefs[j] * h, x_new) - nu[i * cCoefs.size() + j];
                 });
+
+                J1.push_back({});
+                for(std::size_t i1 = 0; i1 < F.size(); ++i1) {
+                    for(std::size_t j1 = 0; j1 < cCoefs.size(); ++j1) {
+                        J1[i * cCoefs.size() + j].push_back([i, j, i1, j1, &J, &cCoefs, h, &x_current_vec, &aCoefs, &t_current](const std::vector<double>& nu){
+                            std::vector<double> x_new;
+                            for(std::size_t i2 = 0; i2 < J.size(); ++i2) {
+                                double sum = 0;
+                                for(std::size_t j2 = 0; j2 < cCoefs.size(); ++j2) {
+                                    sum += aCoefs[j1][j2]*nu[i2 * cCoefs.size() + j2];
+                                }
+                                x_new.push_back(x_current_vec[i2] + h * sum);
+                            }
+                            return (J[i][i1](t_current + cCoefs[j1] * h, x_new) - (i1 == i && j1 == j ? 1 : 0)) * 
+                                            (x_current_vec[i1] + h * aCoefs[j][j1] * nu[i1 * cCoefs.size() + j1]);
+                        });
+                    }
+                }
             }
         }
 
-
-
-        // std::vector<std::vector<double>> res;
-        // for(std::size_t i = 0; i < F.size(); ++i) {
-        //     std::vector<std::function<double(const std::vector<double>&)>> Fnew;
-        //     for(std::size_t j = 0; j < cCoefs.size(); ++j) {
-        //         Fnew.push_back([h, t_current, &x_current_vec, &F, j, &cCoefs, &aCoefs](const std::vector<double>& nu){
-        //             double sum = 0;
-        //             for(std::size_t k = 0; k < cCoefs.size(); ++k) {
-        //                 sum += nu[k] * aCoefs[j][k];
-        //             }    
-        //             return F[j](t_current + h * cCoefs[j], x_current_vec[j] + sum);
-        //             // return 0;
-        //         });
-        //     }
-        // }
-
+        return nm.newton(x_current_vec, F1, J1, eps);
     }
 
 
@@ -64,24 +68,9 @@ class Redo2A {
                                        const std::vector<std::vector<std::function<double(double, const std::vector<double>&)>>>& J,
                                       double eps){
 
-        std::vector<std::function<double(const std::vector<double>&)>> Fnew;
-    
-        for(std::size_t i = 0; i < F.size(); ++i) {
-            Fnew.push_back([h, t_current, &x_current_vec, &F, i](const std::vector<double>& x) {
-                return x_current_vec[i] + h/2*(F[i](t_current, x_current_vec) + F[i](t_current+h, x))-x[i];
-            });
-        }
+        std::vector<double> nu = calculate_next_nu_vec(h, t_current, x_current_vec, F, J, eps);
 
-        std::vector<std::vector<std::function<double(const std::vector<double>&)>>> Jnew;
 
-        for(std::size_t i = 0; i < J.size(); ++i) {
-            Jnew.push_back({});
-            for(std::size_t j = 0; j < J[0].size(); ++j) {
-                Jnew[i].push_back([h, t_current, &x_current_vec, &J, i, j](const std::vector<double>& x) {
-                    return h/2*(J[i][j](t_current, x_current_vec) + J[i][j](t_current+h, x))-(i == j ? 1 : 0);
-                });
-            }
-        }
 
         return nm.newton(x_current_vec, Fnew, Jnew, eps);
     }
